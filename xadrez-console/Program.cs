5acb3fdf10ebc9158fdcd7fa2796c78e666a746b
Joao Lucas <jlucasconrado09@gmail.com>
2022-10-24 16:44:45 -0300

    Imprimindo o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,6 +1,8 @@
-﻿using xadrez_console.tabuleiro;
+﻿using xadrez_console;
+using xadrez_console.tabuleiro;
 // See https://aka.ms/new-console-template for more information
 Tabuleiro tab = new Tabuleiro(8, 8);
+Tela.imprimirTabuleiro(tab);
 Console.ReadLine();
 
 
