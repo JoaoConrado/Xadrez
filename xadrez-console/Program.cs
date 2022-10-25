61d4c8b83090dfa8bb0cf19c9df1933ced8be1ca
Joao Lucas <jlucasconrado09@gmail.com>
2022-10-25 14:59:14 -0300

    Implementando a classe PosicaoXadrez e metodo para converter pos xadrez para uma posicao na matriz

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,17 +3,7 @@ using xadrez_console.tabuleiro;
 using xadrez;
 // See https://aka.ms/new-console-template for more information
 
-try { 
-Tabuleiro tab = new Tabuleiro(8, 8);
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 8));
-tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-Tela.imprimirTabuleiro(tab);
+PosicaoXadrez pos = new PosicaoXadrez('A', 1);
+Console.WriteLine(pos);
+Console.WriteLine(pos.toPosicao());
 Console.ReadLine();
-}
-catch(TabuleiroException e)
-{
-    Console.WriteLine(e.Message);
-}
-
---
+++ xadrez-console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xadrez_console.tabuleiro;
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linhas)
+        {
+            this.Coluna = coluna;
+            this.Linha = linhas;
+        }
+
+
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'A');
+        }
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+
+
+    }
+}
