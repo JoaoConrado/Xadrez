7882f5eb56a3c208c2c633996869d426cdbe1a6a
Joao Lucas <jlucasconrado09@gmail.com>
2022-10-25 15:16:48 -0300

    Melhorando o metodo imprimir tabuleiro (colocando cor nas pecas e letras e numeros do tabuleiro de xadrez)

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -2,8 +2,19 @@
 using xadrez_console.tabuleiro;
 using xadrez;
 // See https://aka.ms/new-console-template for more information
+try { 
+Tabuleiro tab = new Tabuleiro(8, 8);
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-PosicaoXadrez pos = new PosicaoXadrez('A', 1);
-Console.WriteLine(pos);
-Console.WriteLine(pos.toPosicao());
+tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+    
+
+    Tela.imprimirTabuleiro(tab);
 Console.ReadLine();
+}
+catch (TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
